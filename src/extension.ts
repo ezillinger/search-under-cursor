4a7d3b73b5a326abe874b2b752ab4e75fa4a16fb
Eric Zillinger <ezillinger@microsoft.com>
2025-04-29 15:26:11 -0700

    add settings to toggle case sensitivity and whole word support

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.searchUnderCursor', () => {
+export async function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('extension.searchUnderCursor', async () => {
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return;
@@ -12,17 +13,22 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
+		let config = await vscode.workspace.getConfiguration('searchUnderCursor');
+		if (!config) {
+			return;
+		}
+
 		let wordText = editor.document.getText(wordRange);
 
 		vscode.commands.executeCommand('workbench.action.findInFiles', {
 			query: wordText,
 			triggerSearch: true,
-			matchWholeWord: true,
-			isCaseSensitive: true,
+			matchWholeWord: config.get<boolean>('matchWholeWord'),
+			isCaseSensitive: config.get<boolean>('isCaseSensitive'),
 		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
